ad71303e223b4c5d5a4dc928da85c289c0640b9b
moguri846 <dkffkekahsl1@naver.com>
2024-07-14 15:25:02 +0900

    spring boot
    spring boot

---
+++ src/main/java/com/aribnb_clone/AribnbCloneApplication.java
@@ -0,0 +1,13 @@
+package com.aribnb_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AribnbCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AribnbCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aribnb_clone/AribnbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aribnb_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AribnbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
