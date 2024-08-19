238212d8cf23bb68720dc95d4b51c824e30860cf
moguri846 <dkffkekahsl1@naver.com>
2024-08-19 20:58:12 +0900

    feat !: exception 공통 처리 로직 추가
    exception 공통 처리 로직 추가

---
+++ src/main/java/com/airbnb_clone/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.airbnb_clone.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+    // Chatting
+    DUPLICATE_CHAT_ROOM(400, "이미 존재하는 채팅방입니다.");
+
+    private int status;
+    private String message;
+
+    ErrorCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
