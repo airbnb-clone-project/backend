fb96cc25cd983c18859d8e3b2c9ff1a3878201a6
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-08-22 00:07:31 +0900

    feat: 핀 컨트롤러 서비스 정의

---
+++ src/main/java/com/airbnb_clone/pin/controller/PinController.java
@@ -0,0 +1,25 @@
+package com.airbnb_clone.pin.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : com.airbnb_clone.pin.controller
+ * fileName       : PinController
+ * author         : ipeac
+ * date           : 24. 8. 21.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 8. 21.        ipeac       최초 생성
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/pins")
+@Slf4j
+public class PinController {
+
+}
