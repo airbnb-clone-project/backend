b73a3fca99c72830b204bce5ce42ec4226ac0be1
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-08-22 00:07:31 +0900

    feat: 핀 컨트롤러 서비스 정의

---
+++ src/main/java/com/airbnb_clone/pin/service/PinService.java
@@ -0,0 +1,21 @@
+package com.airbnb_clone.pin.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * packageName    : com.airbnb_clone.pin.service
+ * fileName       : PinService
+ * author         : ipeac
+ * date           : 24. 8. 21.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 8. 21.        ipeac       최초 생성
+ */
+@Service
+@RequiredArgsConstructor
+public class PinService {
+
+}
