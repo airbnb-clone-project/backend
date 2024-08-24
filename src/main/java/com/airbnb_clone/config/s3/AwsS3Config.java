88cbd0b32f9e6aa08aea534e42add7d3c91c69f9
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-08-24 23:56:06 +0900

    build: S3 이미지 업로드 관련 localstack 컨테이너 올리기 + S3 설정추가

--- src/main/java/com/airbnb_clone/config/s3/AwsS3Config.java
+++ src/main/java/com/airbnb_clone/config/s3/AwsS3Config.java
@@ -1,8 +1,15 @@
 package com.airbnb_clone.config.s3;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
 
 /**
  * packageName    : com.airbnb_clone.config.s3
@@ -24,9 +31,24 @@ public class AwsS3Config {
     @Value("${AWS_SECRET_KEY}")
     private String secretKey;
 
-    /*@Bean
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Value("${spring.cloud.aws.s3.endpoint}")
+    private String endpoint;
+
+    @Bean
     public S3Client s3Client() {
-        //AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create();
-        return null;
-    }*/
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Client.builder()
+                .region(Region.of(region))
+                .endpointOverride(URI.create(endpoint))
+                .credentialsProvider(() -> awsBasicCredentials)
+                .serviceConfiguration(
+                        S3Configuration.builder()
+                                .pathStyleAccessEnabled(true).
+                                build())
+                .build();
+    }
 }
---
+++ src/main/java/com/airbnb_clone/image/controller/S3ImageController.java
@@ -0,0 +1,31 @@
+package com.airbnb_clone.image.controller;
+
+import com.airbnb_clone.common.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : com.airbnb_clone.image.controller
+ * fileName       : S3ImageController
+ * author         : ipeac
+ * date           : 24. 8. 24.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 8. 24.        ipeac       최초 생성
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/s3/images")
+public class S3ImageController {
+
+    @GetMapping("/pre-signed-url/v1")
+    public ApiResponse<String> getImage() {
+
+        return ApiResponse.of("이미지 URL을 가져오는 API입니다", HttpStatus.OK.value(), "success");
+    }
+}
