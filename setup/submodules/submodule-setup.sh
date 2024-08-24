7393e1dd4a6898fa1de6a56a55fb303be1e6e045
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-08-24 21:13:24 +0900

    build: 권한부여 과정 추가 및 sh 관련 주의점 주석 추가

--- setup/submodules/submodule-setup.sh
+++ setup/submodules/submodule-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#주의점! 해당 스크립트는 LF 로 해석되어야 합니다. ( 인텔리제이에서 CLRF 로 설정되어 있는 경우 변경요망)
 
 echo "프로젝트 루트 디렉토리로 이동 중..."
 cd "$(git rev-parse --show-toplevel)" || { echo "프로젝트 루트 디렉토리로 이동 실패"; exit 1; }
