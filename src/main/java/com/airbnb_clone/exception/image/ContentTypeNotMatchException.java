76eaa2c3d794f961f70cce5964b7ab6b29d1f562
박상준 <qkrtkdwns3410@gmail.com>
2024-08-25 15:50:58 +0900

    feat: 누락된 커밋 추가 s3 관련

---
+++ src/main/java/com/airbnb_clone/exception/image/ContentTypeNotMatchException.java
@@ -0,0 +1,23 @@
+package com.airbnb_clone.exception.image;
+
+import com.airbnb_clone.exception.BusinessException;
+import com.airbnb_clone.exception.ErrorCode;
+
+/**
+ * packageName    : com.airbnb_clone.exception.image
+ * fileName       : ContentTypeNotMatchException
+ * author         : ipeac
+ * date           : 24. 8. 25.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 8. 25.        ipeac       최초 생성
+ */
+public class ContentTypeNotMatchException extends BusinessException {
+    private final String message;
+
+    public ContentTypeNotMatchException(ErrorCode errorCode) {
+        this.message = errorCode.getMessage();
+    }
+}
