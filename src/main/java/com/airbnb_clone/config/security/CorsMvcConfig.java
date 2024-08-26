e943305c665ef6c94effb2ae07152a99740b0fc9
DK <dongwook5478@gmail.com>
2024-08-27 02:18:35 +0900

    feat: mvc cors 추가

---
+++ src/main/java/com/airbnb_clone/config/security/CorsMvcConfig.java
@@ -0,0 +1,36 @@
+package com.airbnb_clone.config.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
+
+/**
+ * packageName    : com.airbnb_clone.config.security
+ * fileName       : CorsMvcConfig
+ * author         : doungukkim
+ * date           : 2024. 8. 25.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024. 8. 25.        doungukkim       최초 생성
+ */
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .exposedHeaders("Set-Cookie")
+                .allowedOrigins("http://localhost:3000");
+    }
+
+}
+
+
