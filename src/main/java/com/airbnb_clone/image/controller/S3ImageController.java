61dbd2ab5ea8846b336a239abf395a24354c23b1
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-08-27 22:42:11 +0900

    feat: presigned 서비스 로직 및 컨트롤러 연결

--- src/main/java/com/airbnb_clone/image/controller/S3ImageController.java
+++ src/main/java/com/airbnb_clone/image/controller/S3ImageController.java
@@ -1,10 +1,13 @@
 package com.airbnb_clone.image.controller;
 
 import com.airbnb_clone.common.global.response.ApiResponse;
+import com.airbnb_clone.image.service.S3ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,10 +25,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/s3/images")
 public class S3ImageController {
+    private final S3ImageService s3ImageService;
 
     @GetMapping("/pre-signed-url/v1")
-    public ApiResponse<String> getImage() {
-
+    public ApiResponse<String> getImage(@RequestParam String contentType) {
+        s3ImageService.generatePresignedUrl(MediaType.valueOf(contentType).toString());
         return ApiResponse.of("이미지 URL을 가져오는 API입니다", HttpStatus.OK.value(), "success");
     }
 }
