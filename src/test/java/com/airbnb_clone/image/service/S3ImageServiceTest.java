545b4cd329d3c7c0ea7bb9bea020a476891c315d
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-08-27 22:27:31 +0900

    test: presined 정상 생성 테스트 구성

--- src/test/java/com/airbnb_clone/image/service/S3ImageServiceTest.java
+++ src/test/java/com/airbnb_clone/image/service/S3ImageServiceTest.java
@@ -1,33 +1,76 @@
 package com.airbnb_clone.image.service;
 
-/*@DataJdbcTestAnnotation
+import com.airbnb_clone.config.s3.AwsS3Config;
+import com.airbnb_clone.exception.image.ContentTypeNotMatchException;
+import org.junit.jupiter.api.*;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 public class S3ImageServiceTest {
 
-    private S3ImageService s3ImageService;
+    private static final DockerImageName LOCALSTACK_IMAGE = DockerImageName.parse("localstack/localstack:3.6");
 
-    @MockBean
+    private static LocalStackContainer localStackContainer;
+
+    private S3ImageService s3ImageService;
     private AwsS3Config s3Config;
 
-    @MockBean
-    private S3Presigner s3Presigner;
+    @BeforeAll
+    public static void setUpAll() {
+        localStackContainer = new LocalStackContainer(LOCALSTACK_IMAGE)
+                .withServices(LocalStackContainer.Service.S3);
+        localStackContainer.start();
+    }
 
     @BeforeEach
     public void setUp() {
-        s3ImageService = new S3ImageService(s3Config, s3Presigner);
+        s3Config = new AwsS3Config();
+        ReflectionTestUtils.setField(s3Config, "accessKey", "test");
+        ReflectionTestUtils.setField(s3Config, "secretKey", "test");
+        ReflectionTestUtils.setField(s3Config, "region", "ap-northeast-2");
+        ReflectionTestUtils.setField(s3Config, "endpoint", localStackContainer.getEndpointOverride(LocalStackContainer.Service.S3).toString());
+        ReflectionTestUtils.setField(s3Config, "bucketName", "test-bucket");
+
+        s3ImageService = new S3ImageService(s3Config);
     }
 
-    @Nested
-    public class PresignedUrlTest {
-        @Test
-        @DisplayName("S3 이미지 URL이 정상으로 생성되는 경우")
-        public void generatePresignedUrl() {
-     *//*       // given
+    @Test
+    @DisplayName("S3 이미지 URL이 정상으로 생성되는 경우")
+    public void generatePresignedUrl() {
+        // given
+        String imageObjectKey = "test";
+        String contentType = "image/png";
 
-            // when
-            String actualPreSignedUrl = s3ImageService.generatePresignedUrl("test", "image/png");
+        // when
+        String actualPreSignedUrl = s3ImageService.generatePresignedUrl(imageObjectKey, contentType);
 
-            // then
-            assertThat(actualPreSignedUrl).isEqualTo("https://test.com");*//*
+        // then
+        assertThat(actualPreSignedUrl).isNotNull();
+        assertThat(actualPreSignedUrl).contains("test-bucket");
+    }
+
+    @Test
+    @DisplayName("S3 이미지 URL이 생성되지 않는 경우 - 컨텐츠 타입 불일치")
+    public void generatePresignedUrlFail() {
+        // given
+        String imageObjectKey = "test";
+        String contentType = "image/jpg";
+
+        // when
+        // then
+        assertThatThrownBy(() -> s3ImageService.generatePresignedUrl(imageObjectKey, contentType))
+                .isInstanceOf(ContentTypeNotMatchException.class)
+                .hasMessage("지원하지 않는 컨텐츠 타입입니다.");
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        if (localStackContainer != null) {
+            localStackContainer.stop();
         }
     }
-}*/
+}
