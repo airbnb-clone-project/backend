e36f7f2469282581a01953b26977e382c868e8f6
박상준 <qkrtkdwns3410@gmail.com>
2024-08-31 16:09:08 +0900

    test: 임시 핀 조회 관련 테스트 코드 작성

--- src/test/java/com/airbnb_clone/pin/service/PinServiceTest.java
+++ src/test/java/com/airbnb_clone/pin/service/PinServiceTest.java
@@ -1,23 +1,28 @@
 package com.airbnb_clone.pin.service;
 
 import com.airbnb_clone.common.testcontainer.MongoDBTestContainer;
+import com.airbnb_clone.config.s3.AwsS3Config;
 import com.airbnb_clone.pin.domain.InnerTempPin;
 import com.airbnb_clone.pin.domain.PinTemp;
 import com.airbnb_clone.pin.domain.dto.request.TemporaryPinCreateRequestDTO;
+import com.airbnb_clone.pin.domain.dto.response.TemporaryPinDetailResponseDTO;
 import com.airbnb_clone.pin.repository.PinRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @DataMongoTest
+@Import(AwsS3Config.class)
 @DisplayName("핀 서비스 테스트")
 @ActiveProfiles("test")
 public class PinServiceTest extends MongoDBTestContainer {
@@ -39,37 +44,74 @@ public class PinServiceTest extends MongoDBTestContainer {
         mt.dropCollection(PinTemp.class);
     }
 
-    @Test
-    @DisplayName("임시 핀 생성 성공 케이스")
-    public void When_createTemporaryPin_Expect_Success() {
-        // given
-        PinTemp pinTemp = PinTemp.of(FirstImageRequestOfFirstUser.getUserNo(), List.of(InnerTempPin.of(FirstImageRequestOfFirstUser.getImageUrl())));
+    @Nested
+    @DisplayName("임시 핀 생성 테스트")
+    class CreateTempPinTest {
+        @Test
+        @DisplayName("임시 핀 생성 성공 케이스")
+        public void When_createTemporaryPin_Expect_Success() {
+            // given
+            InnerTempPin expectedInnerTempPin = InnerTempPin.of(FirstImageRequestOfFirstUser.getImageUrl());
 
-        // when
-        pinService.createTempPin(FirstImageRequestOfFirstUser);
+            PinTemp expectedPinTemp = PinTemp.of(FirstImageRequestOfFirstUser.getUserNo(), Set.of(expectedInnerTempPin));
 
-        // then
-        PinTemp foundTempPin = pinRepository.findPinTempByUserNo(1L).get();
+            // when
+            pinService.createTempPin(FirstImageRequestOfFirstUser);
 
-        assertThat(foundTempPin.getUserNo()).isEqualTo(pinTemp.getUserNo());
-        assertThat(foundTempPin.getInnerTempPins().get(0).getImgUrl()).isEqualTo(pinTemp.getInnerTempPins().get(0).getImgUrl());
+            // then
+            PinTemp foundTempPin = pinRepository.findPinTempByUserNo(1L).get();
+
+            assertThat(foundTempPin.getUserNo()).isEqualTo(expectedPinTemp.getUserNo());
+            assertThat(foundTempPin.getInnerTempPins().stream().findFirst().get().getImgUrl()).isEqualTo(expectedPinTemp.getInnerTempPins().stream().findFirst().get().getImgUrl());
+
+            //내부에 임시 핀 id 가 생성되었는지 확인
+            assertThat(foundTempPin.getInnerTempPins().stream().findFirst().get().get_id()).isNotNull();
+        }
+
+        @Test
+        @DisplayName("이미 존재하는 핀에 임시 핀 추가 시 케이스")
+        public void When_addTemporaryPinToAlreadySavedPin_Expect_Success() {
+            // given
+            InnerTempPin e1 = InnerTempPin.of(FirstImageRequestOfFirstUser.getImageUrl());
+            mt.save(e1);
+
+            PinTemp alreadySavedPin = PinTemp.of(FirstImageRequestOfFirstUser.getUserNo(), Set.of(e1));
+
+            mt.save(alreadySavedPin);
+
+            // when
+            pinService.createTempPin(SecondImageRequestOfFirstUser);
+
+            // then
+            PinTemp foundTempPin = pinRepository.findPinTempByUserNo(1L).get();
+
+            assertThat(foundTempPin.getUserNo()).isEqualTo(alreadySavedPin.getUserNo());
+            assertThat(foundTempPin.getInnerTempPins().size()).isEqualTo(2);
+            assertThat(foundTempPin.getInnerTempPins().stream().skip(1).findFirst().get().getImgUrl()).isEqualTo(SecondImageRequestOfFirstUser.getImageUrl());
+
+            //내부에 임시 핀 id 가 생성되었는지 확인
+            assertThat(foundTempPin.getInnerTempPins().stream().skip(1).findFirst().get().get_id()).isNotNull();
+        }
     }
 
-    @Test
-    @DisplayName("이미 존재하는 핀에 임시 핀 추가 시 케이스")
-    public void When_addTemporaryPinToAlreadySavedPin_Expect_Success() {
-        // given
-        PinTemp alreadySavedPin = PinTemp.of(FirstImageRequestOfFirstUser.getUserNo(), List.of(InnerTempPin.of(FirstImageRequestOfFirstUser.getImageUrl())));
-        mt.save(alreadySavedPin);
+    @Nested
+    @DisplayName("임시 핀 조회 테스트")
+    class GetTempPinTest {
+        @Test
+        @DisplayName("임시 핀 조회 성공 케이스")
+        public void When_getTemporaryPin_Expect_Success() {
+            // given
+            InnerTempPin e1 = InnerTempPin.of(FirstImageRequestOfFirstUser.getImageUrl());
+            mt.save(e1);
 
-        // when
-        pinService.createTempPin(SecondImageRequestOfFirstUser);
+            PinTemp pinTemp = PinTemp.of(FirstImageRequestOfFirstUser.getUserNo(), Set.of(e1));
+            mt.save(pinTemp);
 
-        // then
-        PinTemp foundTempPin = pinRepository.findPinTempByUserNo(1L).get();
+            // when
+            TemporaryPinDetailResponseDTO foundInnerPin = pinService.getTempPin(String.valueOf(pinTemp.getInnerTempPins().stream().findFirst().get().get_id()));
 
-        assertThat(foundTempPin.getUserNo()).isEqualTo(alreadySavedPin.getUserNo());
-        assertThat(foundTempPin.getInnerTempPins().size()).isEqualTo(2);
-        assertThat(foundTempPin.getInnerTempPins().get(1).getImgUrl()).isEqualTo(SecondImageRequestOfFirstUser.getImageUrl());
+            // then
+            assertThat(foundInnerPin.getImgUrl()).isEqualTo(pinTemp.getInnerTempPins().stream().findFirst().get().getImgUrl());
+        }
     }
 }
