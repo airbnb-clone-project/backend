e78d0074ef3e4362e32c102783c452da363a8692
moguri846 <dkffkekahsl1@naver.com>
2024-08-31 18:42:53 +0900

    feat !: LocalDateTimeUtils 추가
    LocalDateTimeUtils 추가

---
+++ src/main/java/com/airbnb_clone/utills/LocalDateTimeUtils.java
@@ -0,0 +1,20 @@
+package com.airbnb_clone.utills;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+public class LocalDateTimeUtils {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public static LocalDateTime parse(String s) {
+        return LocalDateTime.parse(s, FORMATTER);
+    }
+
+    public static String format(LocalDateTime localDateTime) {
+        return localDateTime.format(FORMATTER);
+    }
+}
