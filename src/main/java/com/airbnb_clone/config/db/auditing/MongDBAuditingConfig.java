d05d9d0a7064b63f48dfe6dc04e45e695d548536
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-09-03 23:30:18 +0900

    refactor: 몽고 db auditing 기능 테스트코드 사용을 위한 분리

---
+++ src/main/java/com/airbnb_clone/config/db/auditing/MongDBAuditingConfig.java
@@ -0,0 +1,20 @@
+package com.airbnb_clone.config.db.auditing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+/**
+ * packageName    : com.airbnb_clone.config.db.auditing
+ * fileName       : MongDBAuditingConfig
+ * author         : ipeac
+ * date           : 24. 9. 3.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 9. 3.        ipeac       최초 생성
+ */
+@Configuration
+@EnableMongoAuditing
+public class MongDBAuditingConfig {
+}
