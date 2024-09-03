55a10d70dfe4cc5e877ae69aa9d6472d5cffcb43
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-09-03 23:30:18 +0900

    refactor: 몽고 db auditing 기능 테스트코드 사용을 위한 분리

--- src/main/java/com/airbnb_clone/AirbnbCloneApplication.java
+++ src/main/java/com/airbnb_clone/AirbnbCloneApplication.java
@@ -2,10 +2,8 @@ package com.airbnb_clone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing
 public class AirbnbCloneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AirbnbCloneApplication.class, args);
