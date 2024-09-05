def13a146ca695b241b290b159d108f8402241f7
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-09-05 19:59:02 +0900

    fix: 컨트롤러 클라이언트 응답 코드 반환을 위하여 수정

--- src/main/java/com/airbnb_clone/pin/controller/PinController.java
+++ src/main/java/com/airbnb_clone/pin/controller/PinController.java
@@ -7,7 +7,9 @@ import com.airbnb_clone.pin.domain.dto.response.TemporaryPinsResponseDTO;
 import com.airbnb_clone.pin.service.PinService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,17 +33,23 @@ public class PinController {
     private final PinService pinService;
 
     @GetMapping("/pin/temp/{temp-pin-no}/v1")
-    public ApiResponse<TemporaryPinDetailResponseDTO> getTempPin(@PathVariable("temp-pin-no") String tempPinNo) {
-        return ApiResponse.of("임시핀 조회 성공", HttpStatus.OK.value(), pinService.getTempPin(tempPinNo));
+    public HttpEntity<ApiResponse<TemporaryPinDetailResponseDTO>> getTempPin(@PathVariable("temp-pin-no") String tempPinNo) {
+        return ResponseEntity.ok(
+                ApiResponse.of("임시핀 조회 성공", HttpStatus.OK.value(), pinService.getTempPin(tempPinNo))
+        );
     }
 
     @GetMapping("/pin/temps/{user-no}/v1")
-    public ApiResponse<List<TemporaryPinsResponseDTO>> getTempPins(@PathVariable("user-no") Long userNo) {
-        return ApiResponse.of("임시핀 목록 조회 성공", HttpStatus.OK.value(), pinService.getTempPins(userNo));
+    public HttpEntity<ApiResponse<List<TemporaryPinsResponseDTO>>> getTempPins(@PathVariable("user-no") Long userNo) {
+        return ResponseEntity.ok(
+                ApiResponse.of("임시핀 목록 조회 성공", HttpStatus.OK.value(), pinService.getTempPins(userNo))
+        );
     }
 
     @PostMapping("/pin/temp/v1")
-    public ApiResponse<String> uploadImage(@RequestBody TemporaryPinCreateRequestDTO temporaryPinCreateRequestDTO) {
-        return ApiResponse.of("핀이 정상적으로 임시저장되었습니다.", HttpStatus.OK.value(), pinService.createTempPin(temporaryPinCreateRequestDTO).toString());
+    public HttpEntity<ApiResponse<String>> uploadImage(@RequestBody TemporaryPinCreateRequestDTO temporaryPinCreateRequestDTO) {
+        return ResponseEntity.ok(
+                ApiResponse.of("핀이 정상적으로 임시저장되었습니다.", HttpStatus.OK.value(), pinService.createTempPin(temporaryPinCreateRequestDTO).toString())
+        );
     }
 }
