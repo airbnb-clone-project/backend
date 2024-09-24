78bf29fa1593a7d27d9467a77206ab4dac550b06
DK <dongwook5478@gmail.com>
2024-09-24 16:31:56 +0900

    feat: presigned url 발급기능 추가

--- src/main/java/com/airbnb_clone/auth/service/ProfileImageService.java
+++ src/main/java/com/airbnb_clone/auth/service/ProfileImageService.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.View;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
@@ -35,11 +36,10 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ProfileImageService {
     private final AwsS3Config awsS3Config;
-    private final S3Client s3Client;
     private final UserRepository userRepository;
 
     // S3 에 이미지를 업로드 할 수 있는 사전 서명된 URL을 생성
-    public String generatePresignedUrl(String contentType) {
+    public ResponseEntity<?> generatePresignedUrl(String contentType) {
         ContentTypeValidator.isValidImageContentType(contentType);
         // S3Presigner : AWS 자격 증명 없이 S3 버킷에 대해 특정 작업(파일 업로드, 다운로드) 수행 가능
         try (S3Presigner presigner = S3Presigner.builder()
@@ -52,7 +52,17 @@ public class ProfileImageService {
             PutObjectPresignRequest presignRequest = getPutObjectPresignRequest(contentType, uniqueKey);
 
             PresignedPutObjectRequest presignedPutObjectRequest = presigner.presignPutObject(presignRequest);
-            return presignedPutObjectRequest.url().toString();
+
+            String presignedUrl = presignedPutObjectRequest.url().toString();
+
+            Map<String, Object> json = new HashMap<>();
+            json.put("status", 200);
+            json.put("message", "presigned Url 이 생성되었습니다.");
+            json.put("presignedUrl", presignedUrl);
+
+            return ResponseEntity
+                    .ok()
+                    .body(json);
         }
     }
 
@@ -101,11 +111,6 @@ public class ProfileImageService {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(json);
-    }
-
-    // url에서 key 추출
-    private String extractKeyFromUrl(String url) {
-        return url.substring(url.lastIndexOf("/") + 1);
     }
 
     // S3에 객체를 업로드하기 위한 사전 서명된 URL을 생성하는 데 필요한 요청 객체 생성
