35e7b8e6e78d45cea4d040e47b17f36a1e44461d
Yea-Na <tlsdpsk1992@naver.com>
2024-09-24 20:58:52 +0900

    fix : Controller -> RestController 변경 외 공백제거 등

--- src/main/java/com/airbnb_clone/detaile/controller/DetailPageController.java
+++ src/main/java/com/airbnb_clone/detaile/controller/DetailPageController.java
@@ -2,14 +2,12 @@ package com.airbnb_clone.detaile.controller;
 
 
 import com.airbnb_clone.detaile.dto.DetailPageDto;
-import com.airbnb_clone.detaile.dto.PinLikeDto;
 import com.airbnb_clone.detaile.service.DetailPageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,11 +19,5 @@ public class DetailPageController {
     public DetailPageDto getDetailPage(@PathVariable Long pinId) {
         DetailPageDto detailPageDto = detailPageService.getDetailPage(pinId);
         return detailPageDto;
-    }
-
-    @GetMapping("api/pin/like/{pinId}/v1")
-    public List<PinLikeDto> getDetailPageLike(@PathVariable Long pinId) {
-        List<PinLikeDto> pinLikes = detailPageService.getPinLikes(pinId);
-        return pinLikes;
     }
 }
