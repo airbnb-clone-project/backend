cc886b8b7cd46c80f7244b97e56322c333e1c237
Yea-Na <tlsdpsk1992@naver.com>
2024-09-26 03:09:05 +0900

    feat : 핀반응 목록 출력

--- src/main/java/com/airbnb_clone/detaile/repository/DetailPageRepository.java
+++ src/main/java/com/airbnb_clone/detaile/repository/DetailPageRepository.java
@@ -21,20 +21,21 @@ public class DetailPageRepository {
 
     // 특정 핀의 페이지 정보 호출
     public DetailPageDto findPageById(Long pinId) {
-        String sql = "SELECT " +
-                "p.no AS pinNo, " +
-                "p.img_url, " +
-                "p.title, " +
-                "p.description, " +
-                "p.link, " +
-                "u.no AS userNo, " +
-                "u.first_name, " +
-                "u.last_name, " +
-                "u.profile_img_url " +
-                "FROM pin p " +
-                "JOIN board b ON p.board_no = b.no " +
-                "JOIN users u ON b.user_no = u.no " +
-                "WHERE p.no = ?";
+        String sql = """
+                SELECT \
+                p.no AS pinNo, \
+                p.img_url, \
+                p.title, \
+                p.description, \
+                p.link, \
+                u.no AS userNo, \
+                u.first_name, \
+                u.last_name, \
+                u.profile_img_url \
+                FROM pin p \
+                JOIN board b ON p.board_no = b.no \
+                JOIN users u ON b.user_no = u.no \
+                WHERE p.no = ?""";
 
         return jdbcTemplate.queryForObject(sql, new Object[]{pinId}, new RowMapper<DetailPageDto>() {
             @Override
@@ -58,20 +59,21 @@ public class DetailPageRepository {
 
     //특정 핀의 핀 반응 목록 호출
     public List<PinLikeDto> findPinlikesById(Long pinId){
-        String sql = "SELECT " +
-                "pl.no AS pinLikeNo, " +
-                "pl.target_pin_no, " +
-                "pl.emoji_no " +
-                "pl.created_at " +
-                "pl.liker " +
-                "u.first_name, " +
-                "u.last_name, " +
-                "u.profile_img_url " +
-                "FROM pin_like pl " +
-                "JOIN pin p ON pl.target_pin_no = p.no " +
-                "JOIN users u ON pl.liker = u.no " +
-                "WHERE p.no = ? " +
-                "ORDER BY pl.created_at DESC ";
+        String sql = """
+                SELECT \
+                pl.no AS pinLikeNo, \
+                pl.target_pin_no, \
+                pl.emoji_no, \
+                pl.created_at, \
+                pl.liker, \
+                u.first_name, \
+                u.last_name, \
+                u.profile_img_url \
+                FROM pin_like pl \
+                JOIN pin p ON pl.target_pin_no = p.no \
+                JOIN users u ON pl.liker = u.no \
+                WHERE p.no = ? \
+                ORDER BY pl.created_at DESC\s""";
 
         return jdbcTemplate.query(sql, new Object[]{pinId}, new RowMapper<PinLikeDto>() {
                     @Override
