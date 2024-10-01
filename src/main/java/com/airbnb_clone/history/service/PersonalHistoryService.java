35dc958031724db7108a21870149f3620c5048e0
박상준 <qkrtkdwns3410@gmail.com>
2024-10-01 18:37:57 +0900

    feat: 레디스 사용자 방문 히스토리 제공

--- src/main/java/com/airbnb_clone/history/service/PersonalHistoryService.java
+++ src/main/java/com/airbnb_clone/history/service/PersonalHistoryService.java
@@ -4,10 +4,13 @@ import com.airbnb_clone.history.domain.PersonalHistoryHash;
 import com.airbnb_clone.history.repository.PersonalHistoryRedisRepository;
 import com.airbnb_clone.pin.domain.pin.dto.response.PinHistoryResponseDTO;
 import com.airbnb_clone.pin.repository.PinMySQLRepository;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * packageName    : com.airbnb_clone.history.service
@@ -26,6 +29,24 @@ public class PersonalHistoryService {
     public static final int HISTORY_LIMIT = 10;
     private final PersonalHistoryRedisRepository personalHistoryRedisRepository;
     private final PinMySQLRepository pinMySQLRepository;
+
+    /**
+     * 사용자의 분류별 카운트를 제공한다. 키 : 밸류 구조로 각 classification 별로 카운트를 제공한다.
+     * @return 분류별 카운트
+     */
+    public Map<String, Integer> getHistoryCounts(@NonNull Long userNo) {
+        List<PersonalHistoryHash> foundHistories = List.copyOf(personalHistoryRedisRepository.findByUserNoOrderByVisitedAtDesc(userNo));
+
+        final Map<String, Integer> historyCounts = new HashMap<>();
+
+        //각 방문이력 분류별 카운트를 합산한다.
+        for (PersonalHistoryHash foundHistory : foundHistories) {
+            String classification = foundHistory.getVisitedHistoryClassification();
+            historyCounts.put(classification, historyCounts.getOrDefault(classification, 0) + 1);
+        }
+
+        return historyCounts;
+    }
 
     public void saveHistory(Long userNo, Long pinNo) {
         PinHistoryResponseDTO foundPin = pinMySQLRepository.findPinForHistoryByNo(pinNo);
