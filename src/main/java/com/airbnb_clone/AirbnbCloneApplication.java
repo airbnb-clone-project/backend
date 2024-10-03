88a51ac78aeeaa4695e47814040110dfaa193860
DK <dongwook5478@gmail.com>
2024-10-03 14:13:30 +0900

    feat: definit url in Application.java

--- src/main/java/com/airbnb_clone/AirbnbCloneApplication.java
+++ src/main/java/com/airbnb_clone/AirbnbCloneApplication.java
@@ -1,9 +1,17 @@
 package com.airbnb_clone;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "http://localhost:8008", description = "local server"),
+				@Server(url = "http://34.171.110.81:8008", description = "swagger server")
+		}
+)
 public class AirbnbCloneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AirbnbCloneApplication.class, args);
